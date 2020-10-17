2f1ab49e0e68c0cbb204023d65c1d0846e4c1115
katherinepeterson <katherine.peterson@yahoo.com>
2020-10-16 21:30:38 -0700

    rm more unused files and add styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
