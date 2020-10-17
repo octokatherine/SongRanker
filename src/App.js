4ae9f9d7fb30c531fa2e3560af3bdc3aa6277507
katherinepeterson <katherine.peterson@yahoo.com>
2020-10-16 22:21:12 -0700

    add spotify auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, { Component } from 'react'
+import Home from './Home'
+import axios from 'axios'
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
+export const authEndpoint = 'https://accounts.spotify.com/authorize'
+const clientId = 'f170a7aa1b8e4e11ae2e80cbbc695b31'
+const redirectUri = 'http://localhost:3000/'
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-playback-state',
+  'streaming',
+  'user-top-read',
+  'user-library-read',
+  'playlist-read-private',
+  'playlist-modify-private',
+]
 
-function App() {
-  return (
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
-  );
+const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split('=')
+      initial[parts[0]] = decodeURIComponent(parts[1])
+    }
+    return initial
+  }, {})
+window.location.hash = ''
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      token: null,
+    }
+  }
+
+  componentDidMount() {
+    let _token = hash.access_token
+    if (_token) {
+      this.setState({
+        token: _token,
+      })
+    }
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + _token
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                '%20'
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
+          )}
+          {this.state.token && <Home token={this.state.token} />}
+        </header>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+const Home = ({ token }) => {
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10'
+      )
+      .then(function (response) {
+        console.log(response)
+      })
+  }, [])
+
+  return <h1>Welcome!</h1>
+}
+
+export default Home
