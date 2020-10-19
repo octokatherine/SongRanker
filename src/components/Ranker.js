a7f81da1688cb7b817fad4039daac29a6b783116
katherinepeterson <katherine.peterson@yahoo.com>
2020-10-18 23:22:26 -0700

    start ranker

--- src/components/Ranker.js
+++ src/components/Ranker.js
@@ -3,7 +3,27 @@ import axios from 'axios'
 import styled from 'styled-components'
 import { Subheader, Text, PrimaryButton } from '../Base'
 
+function Comparison(g, l, gIndex, lIndex, clicked) {
+  this.greater = g
+  this.lesser = l
+  this.greaterIndex = gIndex
+  this.lesserIndex = lIndex
+  this.clicked = clicked
+}
+
 const Ranker = ({ setScreen, songs, setSongs, albums }) => {
+  const [ranked, setRanked] = useState([])
+  const [comparisons, setComparisons] = useState([])
+  const [highestIndex, setHighestIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(1)
+  const [length, setLength] = useState(0)
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    setItems(songs)
+    setLength(songs.length)
+  }, [songs])
+
   useEffect(() => {
     albums.forEach((a) => {
       axios
@@ -16,21 +36,99 @@ const Ranker = ({ setScreen, songs, setSongs, albums }) => {
     })
   }, [])
 
+  const onSelectLeft = () => {
+    compare(highestIndex, currentIndex, true)
+  }
+
+  const onSelectRight = () => {
+    compare(currentIndex, highestIndex, true)
+  }
+
+  const rank = (index) => {
+    if (length <= 0) return
+    setRanked((prev) => [...prev, items[index]])
+    setLength(items.length - 1)
+    setItems((prev) => {
+      const newState = prev
+      newState.splice(index, 1)
+      return newState
+    })
+    setHighestIndex(0)
+    setCurrentIndex(1)
+  }
+
+  const greaterSearch = (comps, curr, target, i) => {
+    var comp,
+      found = false
+
+    while (!found && i >= 0) {
+      comp = comps[i]
+      if (comp.greater === curr) {
+        found = comp.lesser === target || greaterSearch(comps, comp.lesser, target, i)
+      }
+      i--
+    }
+
+    return found
+  }
+
+  const greaterThan = (aIndex, bIndex) => {
+    return greaterSearch(comparisons, items[aIndex], items[bIndex], comparisons.length - 1)
+  }
+
+  const displayNext = () => {
+    if (length < 1) {
+      setScreen('results')
+    } else if (currentIndex < length) {
+      // check if a comparison can be inferred
+      if (greaterThan(currentIndex, highestIndex)) {
+        compare(currentIndex, highestIndex)
+      } else if (greaterThan(highestIndex, currentIndex)) {
+        compare(highestIndex, currentIndex)
+      }
+    } else {
+      rank(highestIndex)
+      displayNext()
+    }
+  }
+
+  const compare = (hIndex, lIndex, clicked) => {
+    var comp = new Comparison(items[hIndex], items[lIndex], hIndex, lIndex, clicked)
+    setComparisons((prev) => [...prev, comp])
+
+    if (hIndex !== highestIndex) {
+      setHighestIndex(hIndex)
+    }
+
+    setCurrentIndex((prev) => prev + 1)
+    displayNext()
+  }
+
+  const leftSong = items[highestIndex]
+  const rightSong = items[currentIndex]
+
+  console.log('ranked :>> ', ranked)
+  console.log('highestIndex :>> ', highestIndex)
+  console.log('currentIndex :>> ', currentIndex)
+  console.log('length :>> ', length)
+  console.log('items :>> ', items)
+  console.log('length :>> ', length)
+  console.log('comparisons :>> ', comparisons)
   return (
     <Container>
       <Heading>Choose a song</Heading>
       {songs.length && (
         <Options>
-          <div>
-            <AlbumArtwork src={songs[0].image_url} />
-            <TitleText>{songs[0]?.name}</TitleText>
-            <ArtistText>{songs[0]?.artists[0].name}</ArtistText>
+          <div onClick={onSelectLeft}>
+            <AlbumArtwork src={leftSong?.image_url} />
+            <TitleText>{leftSong?.name}</TitleText>
+            <ArtistText>{leftSong?.artists[0].name}</ArtistText>
           </div>
           <OrText>OR</OrText>
-          <div>
-            <AlbumArtwork src={songs[0].image_url} />
-            <TitleText>{songs[1]?.name}</TitleText>
-            <ArtistText>{songs[1]?.artists[0].name}</ArtistText>
+          <div onClick={onSelectRight}>
+            <AlbumArtwork src={leftSong?.image_url} />
+            <TitleText>{rightSong?.name}</TitleText>
+            <ArtistText>{rightSong?.artists[0].name}</ArtistText>
           </div>
         </Options>
       )}
