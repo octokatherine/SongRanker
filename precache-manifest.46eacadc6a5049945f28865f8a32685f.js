92acdf34385acd50b7174464b92e2b2ce3352cd7
katherinepeterson <katherine.peterson@yahoo.com>
2020-10-20 23:19:51 -0700

    Updates

---
+++ precache-manifest.46eacadc6a5049945f28865f8a32685f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ea6044fe66b98318577762cf7e45714",
+    "url": "/rankify/index.html"
+  },
+  {
+    "revision": "0b54083d0b18498c6165",
+    "url": "/rankify/static/js/2.31a64a4e.chunk.js"
+  },
+  {
+    "revision": "1af2e467dd1dd801ef7041afe25404ab",
+    "url": "/rankify/static/js/2.31a64a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88edac4e33118c356eef",
+    "url": "/rankify/static/js/main.ab97fb32.chunk.js"
+  },
+  {
+    "revision": "f8779f65072a0a249586",
+    "url": "/rankify/static/js/runtime-main.a2a647bf.js"
+  },
+  {
+    "revision": "b4ef988673a410c6b9bc26c791c16537",
+    "url": "/rankify/static/media/down.b4ef9886.svg"
+  },
+  {
+    "revision": "4121971b84ae72fcf9897625e67b4188",
+    "url": "/rankify/static/media/logo.4121971b.svg"
+  },
+  {
+    "revision": "dc035f9ef630bfe33fcadf53d1914a36",
+    "url": "/rankify/static/media/magnify.dc035f9e.svg"
+  }
+]);
